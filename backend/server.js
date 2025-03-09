a0140fe4d91581a54bebb137b2a08acac3ea1a9d
BonamGokulVenkat <bonamgokul@gmail.com>
2025-03-09 10:12:37 +0530

    Start commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
 
-const PORT = 3000;
+dotenv.config();
 
+const PORT = process.env.PORT||5000;
 const app = express();
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 app.listen(PORT,()=>{
     console.log(`Running on : ${PORT}`);
